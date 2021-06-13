1d113b2550e471af02c4cd35a2f2f68391af9815
Jordan-Edwards <jordanedwards441@gmail.com>
2021-06-13 17:04:02 -0500

    fixed filepaths

---
+++ Robots.py
@@ -0,0 +1,7 @@
+class robots:
+    def __init__(self, health, power_level, weapon, attack_power):
+        self.health = 100
+        self.power_level = 1
+        self.attack_power = 5
+        self.weapon = bool
+
--- robot.py
+++
@@ -1,5 +0,0 @@
-class robot:
-    def __init__(self):
-        self.name = ""
-        self.health = 100
-        self.power_level = 1
